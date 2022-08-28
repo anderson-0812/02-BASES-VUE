7d4c0e2bef40670bc78823cfb342f8afc06c96a1
Anderson E. Jimenez Prieto <andersonmurdog@gmail.com>
2022-08-28 17:48:00 -0500

    directiva v-for desestructuracion y sacar index

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 console.log( Vue );
 
+const quotes = [
+    { quote: 'The night is darkest just before the dawn. And I promise you, the dawn is coming.', author: 'Harvey Dent, The Dark Knight' },
+    { quote: 'I believe what doesn’t kill you simply makes you, stranger.', author: 'The Joker, The Dark Knight' },
+    { quote: 'Your anger gives you great power. But if you let it, it will destroy you… As it almost did me', author: 'Henri Ducard, Batman Begins' },
+    { quote: 'You either die a hero or live long enough to see yourself become the villain.', author: 'Harvey Dent, The Dark Knight' },
+    { quote: 'If you’re good at something, never do it for free.', author: 'The Joker, The Dark Knight' },
+    { quote: 'Yes, father. I shall become a bat.', author: 'Bruce Wayne/Batman, Batman: Year One' },
+]
+
+console.log(quotes);
+
 // todo lo que esta en app es un component 
 const app = Vue.createApp({
     
@@ -7,26 +18,10 @@ const app = Vue.createApp({
     // message o mas opciones podran ser usadas como variables desde el html
     data() {
         return {
-            message: 'Soy Batman',
-            author: 'Burce way'
+            quotes
         }
     },
 
-    // uso de metodos
-    methods:{
-        changeQuote( event ){
-            console.log("hola desde metodo", event);
-            // con this hacemos referencia a nuestras variables y metodos
-            this.author = 'Anderson Jimenez'
-            this.message = 'Yo soy Ironman'
-
-            this.capitalizar()
-
-        },
-        capitalizar() {
-            this.message = this.message.toUpperCase()
-        }
-    }
 })
 
 
